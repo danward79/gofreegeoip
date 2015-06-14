d4e30b83c407ed62b8d6df752685c097b675e0a9
Dan Ward <danward79@googlemail.com>
2015-06-15 06:27:17 +1000

    Ready Update

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# gofreegeoip
-Provides aGo Freegeoip 
+gofreegeoip
+===========
+
+Provides access to [Freegeoip.net](https://freegeoip.net/?q=8.8.8.8) using GO
+
+Freegeoip provides an API which can give results via, XML, JSON and CSV. This app uses the JSON interface.
+
+freegeoip.net/{format}/{IP_or_hostname}
+
+You are limited to *10,000* calls per hour. If this is not enough you have to provide your own server! ;-) In which case see the [Github project here.](https://github.com/fiorix/freegeoip)
+
+TODO - Add 403 Support TODO - Quiery Timeout handling
--- main/main.go
+++ main/main.go
@@ -10,8 +10,8 @@ import (
 func main() {
 
 	//Command line variables
-	server := flag.String("s", "freegeoip.net", "Enter the IP address for the quiery server, leave blank for freegeoip.net.")
-	ip := flag.String("a", "", "Enter the IP address for the location you wish to quiery, leave blank for this WAN IP.")
+	server := flag.String("s", "freegeoip.net", "Enter the IP address (IP:PORT) for the quiery server, leave blank for freegeoip.net.")
+	ip := flag.String("a", "", "Enter the IP address for the location you wish to quiery, leave blank for this External WAN IP.")
 	flag.Parse()
 
 	loc := gofreegeoip.Quiery(*server, *ip)
